8aef8e79df288ce59cff202837a7f72ebfdf17b5
lijix <3034089596@qq.com>
2017-07-27 08:49:17 +0800

    业务对接，三表合一

---
+++ NIC-wechat/src/main/java/com/litt/nic/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.litt.nic.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.litt.nic.Dao.userMapper;
+import com.litt.nic.pojo.user;
+import com.litt.nic.service.IUserService;
+@Service
+public  class UserServiceImpl implements IUserService {
+
+	@Autowired
+	private userMapper usermapper;
+	@Override
+	public user findById(int id) {
+		
+		return usermapper.selectByPrimaryKey(id);
+	}
+
+}
